6b2b0fd5f8a34e99610b5a210ab42d4c4575d316
Eduard Cruz <69446125+Dev-eCorp@users.noreply.github.com>
2023-05-17 00:47:46 -0500

    Revert "Feature/login funtionality"

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: ["upload.wikimedia.org", "www.dominio2.com", "www.google.com"],
-  },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
